cc384e715a02953a88237f6dc05cc4fc179b0d0c
ClaireH <55714299+ClaireHien@users.noreply.github.com>
2026-03-04 10:32:08 +0100

    seeder add4 fanzine

--- database/seeders/FanzineSeeder.php
+++ database/seeders/FanzineSeeder.php
@@ -17,6 +17,30 @@ class FanzineSeeder extends Seeder
             ['name' => 'L\'Orc : Origines', 'number' => 2, 'description' => 'Spécial web-série Evil Ed et bande dessinée.', 'price' => 4.50],
             ['name' => 'Riflesso di un Coltello', 'number' => 3, 'description' => 'Focus sur le cinéma d\'horreur italien.', 'price' => 6.00],
             ['name' => 'Brume Émeraude', 'number' => 4, 'description' => 'Le guide officiel du Petit Festival de l\'Épouvante.', 'price' => 5.90],
+            [
+                'name' => 'Les Griffes du Giallo',
+                'number' => 5,
+                'price' => 12,
+                'description' => 'Un voyage sanglant à travers le cinéma de Mario Bava et Dario Argento.',
+            ],
+            [
+                'name' => 'Manoirs Hantés',
+                'number' => 6,
+                'price' => 10,
+                'description' => 'Étude architecturale des demeures les plus terrifiantes du septième art.',
+            ],
+            [
+                'name' => 'Technicolor Macabre',
+                'number' => 7,
+                'price' => 15,
+                'description' => 'L\'esthétique de la Hammer et l\'usage de la couleur dans l\'épouvante britannique.',
+            ],
+            [
+                'name' => 'Grimoires Oubliés',
+                'number' => 8,
+                'price' => 12,
+                'description' => 'Focus sur la littérature fantastique du XIXème siècle et ses adaptations.',
+            ],
         ];
 
         foreach ($fanzines as $f) {
